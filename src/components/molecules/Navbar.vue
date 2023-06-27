59d19793cdcf3a69cdb1e5c746b37460b41bf482
Filippo Di Marco <filidm5795@gmail.com>
2023-06-27 12:03:13 +0200

    update navbar

---
+++ src/components/molecules/Navbar.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+import Social from './Socials.vue'
+const menu = ref([
+  { title: "Home Page", href: './index.html' },
+  { title: "Project", href: './project.html' },
+  { title: "Out in the World", href: './outintheworld.html' },
+  { title: "Showcase", href: './showcase.html' },
+  { title: "Multiverse", href: './multiverse.html' },
+  { title: "Governance", href: './governance.html' }
+])
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid" style="max-width: 1400px;">
+      <div class="d-flex align-items-center">
+        <a class="navbar-brand" href="./index.html">
+          <img src="../../assets/img/mapod_new_render_scontornato.png" alt="" width="50" height="50"
+            class="d-inline-block align-text-top">
+        </a>
+        <a class="navbar-brand fw-semibold" style="font-weight: 700; color:#4f41d1; padding-bottom: 10px;"
+          href="./index.html">MAPOD4D</a>
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-lg-flex justify-content-lg-end" id="navbarNav">
+        <ul class="navbar-nav gap-lg-3" style="font-size: 1.2rem; font-weight:bold;">
+          <li v-for="(item, i) in menu" :key="i + item" class="nav-item text-center text-uppercase text-nowrap">
+            <a class="nav-link fw-normal fs-6 text-primary" aria-current="page" :href="item.href">{{ item.title }}</a>
+          </li>
+        </ul>
+        <div id="socials" class="d-xl-flex gap-2 d-none ms-3">
+          <Social />
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div id="socials" class="d-flex gap-2 d-xl-none justify-content-center my-2">
+    <Social />
+  </div>
+</template>
+
+<style scoped>
+.fs-6 {
+  font-size: 0.9rem !important;
+}
+</style>
---
+++ src/components/molecules/Socials.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+const socials = ref([
+  { social: "fab fa-whatsapp", href: 'https://chat.whatsapp.com/FdmQ0yq7Sk2HiPmZCE4JUP' },
+  { social: "fab fa-discord", href: 'https://discord.gg/ns4GGuVG5Y' },
+  { social: "fab fa-facebook", href: 'https://www.facebook.com/mapod4d' },
+  { social: "fab fa-youtube", href: 'https://www.youtube.com/@mapod4d' },
+  { social: "fab fa-github", href: 'https://github.com/mapod4d/' },
+  { social: "fab fa-twitch", href: 'https://www.twitch.tv/mapod4d' }
+])
+</script>
+
+<template>
+  <a v-for="(item, i) in  socials " :key="i + item"
+    class="btn btn-outline-dark fs-6 d-flex justify-content-center align-items-center" role="button" :href="item.href"
+    target="_blank">
+    <i :class="item.social"></i>
+  </a>
+</template>
+
+<style scoped>
+div#socials>a {
+  width: 35px;
+  height: 35px;
+  border: 1.5px solid #000000;
+  border-radius: 17.5px;
+  cursor: pointer;
+  color: #000000;
+  transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+}
+
+div#socials>a:hover {
+  background-color: #000000;
+  color: #ffffff;
+}
+
+div#socials>a:focus {
+  color: #ffffff !important;
+}
+</style>
